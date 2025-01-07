2b13f3cf83eb3d1dc5e1423897757bb039fa4ea5
DerryCody <derrycody356@gmail.com>
2025-01-07 17:31:47 +0000

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,15 @@
 import tkinter 
+import tkinter.messagebox
 
 screen=tkinter.Tk()
 screen.geometry("619x700")
 
 screen.title("Tic-Tac-Toe")
 screen.configure(background="blue")
-p1score=0
-p2score=0
 availableplaces=["","","","","","","","",""]
 next=1
+possiblewins=[(0,1,2),(0,4,8),(8,5,2),(8,7,6),(0,4,8),(2,4,6),(3,4,5),(1,4,7)]
+
 
 def p1place(buttonchosen,buttonpos):
     global next
@@ -19,7 +20,18 @@ def p1place(buttonchosen,buttonpos):
     elif next==2 and availableplaces[buttonpos-1]=="":
         buttonchosen.config(text="O")
         availableplaces[buttonpos-1]="O"
-        next=next-1        
+        next=next-1   
+    winner()     
+
+def winner():
+    for pss in possiblewins:
+      if "X" == availableplaces[pss[0]] and "X" == availableplaces[pss[1]] and "X" == availableplaces[pss[2]]:
+          tkinter.messagebox.showinfo("Hi","Player 1 wins!")
+          screen.destroy()
+      if "O" == availableplaces[pss[0]] and"O" == availableplaces[pss[1]] and "O" == availableplaces[pss[2]]:
+          tkinter.messagebox.showinfo("Hi","Player 2 wins!")
+          screen.destroy()
+    print(availableplaces)
  
 
 P1=tkinter.Label(screen,background="grey",text="Player 1 : X",font=("times new roman",50))
@@ -29,20 +41,21 @@ P2.grid(row=1,column=1)
 B1=tkinter.Button(screen,background="white",font=("times new roman",30),width=10,height=5,command=lambda:p1place(B1,1))
 B1.grid(row=2,column=0)
 B2=tkinter.Button(screen,background="white",font=("times new roman",30),width=10,height=5,command=lambda:p1place(B2,2))
-B2.grid(row=3,column=0)
+B2.grid(row=2,column=1)
 B3=tkinter.Button(screen,background="white",font=("times new roman",30),width=10,height=5,command=lambda:p1place(B3,3))
-B3.grid(row=4,column=0)
+B3.grid(row=2,column=2)
 B4=tkinter.Button(screen,background="white",font=("times new roman",30),width=10,height=5,command=lambda:p1place(B4,4))
-B4.grid(row=2,column=1)
+B4.grid(row=3,column=0)
 B5=tkinter.Button(screen,background="white",font=("times new roman",30),width=10,height=5,command=lambda:p1place(B5,5))
 B5.grid(row=3,column=1)
 B6=tkinter.Button(screen,background="white",font=("times new roman",30),width=10,height=5,command=lambda:p1place(B6,6))
-B6.grid(row=4,column=1)
+B6.grid(row=3,column=2)
 B7=tkinter.Button(screen,background="white",font=("times new roman",30),width=10,height=5,command=lambda:p1place(B7,7))
-B7.grid(row=2,column=2)
+B7.grid(row=4,column=0)
 B8=tkinter.Button(screen,background="white",font=("times new roman",30),width=10,height=5,command=lambda:p1place(B8,8))
-B8.grid(row=3,column=2)
+B8.grid(row=4,column=1)
 B9=tkinter.Button(screen,background="white",font=("times new roman",30),width=10,height=5,command=lambda:p1place(B9,9))
 B9.grid(row=4,column=2)
+
 
 screen.mainloop()
